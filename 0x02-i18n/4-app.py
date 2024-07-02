3a0ad71b266ef6aeb43bc38f2cfa102c54f4192a
Bart254 <omondibartlett3@gmail.com>
2024-07-02 21:04:57 +0000

    add 4-app.py

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+""" language translation module """
+from flask import (Flask, render_template, request)
+from flask_babel import Babel
+from typing import List
+
+
+class Config:
+    """ app config class """
+    LANGUAGES: List[str] = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE: str = "en"
+    BABEL_DEFAULT_TIMEZONE: str = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """ returns language to display """
+    locale = request.args.get("locale")
+    if locale and locale in app.config["LANGUAGES"]:
+        return locale
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """ root url """
+    return render_template('4-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
